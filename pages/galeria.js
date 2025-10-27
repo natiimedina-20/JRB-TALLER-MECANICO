39375811e6b12401bbcd785226e4e3f922148cd8
nati <natiimedina.20@gmail.com>
2025-10-27 20:22:54 -0300

    fix: images path

--- pages/galeria.js
+++ pages/galeria.js
@@ -1,12 +1,12 @@
 const imagenesIndex = [
-  { id: 1, nombre: "", image: "../assets/1.jpg" },
-  { id: 2, nombre: "", image: "../assets/2.jpg" },
-  { id: 3, nombre: "", image: "../assets/3.jpg" },
-  { id: 4, nombre: "", image: "../assets/4.jpg" },
-  { id: 5, nombre: "", image: "../assets/5.jpg" },
-  { id: 6, nombre: "", image: "../assets/6.jpg" },
-  { id: 7, nombre: "", image: "../assets/7.jpg" },
-  { id: 8, nombre: "", image: "../assets/8.jpg" }
+  { id: 1, nombre: "", image: "./assets/1.jpg" },
+  { id: 2, nombre: "", image: "./assets/2.jpg" },
+  { id: 3, nombre: "", image: "./assets/3.jpg" },
+  { id: 4, nombre: "", image: "./assets/4.jpg" },
+  { id: 5, nombre: "", image: "./assets/5.jpg" },
+  { id: 6, nombre: "", image: "./assets/6.jpg" },
+  { id: 7, nombre: "", image: "./assets/7.jpg" },
+  { id: 8, nombre: "", image: "./assets/8.jpg" }
 ];
 
 export default imagenesIndex;
