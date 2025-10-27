44e7d178f8c92ef2a1b2cfb4c8d0ca2c101b1a90
nati <natiimedina.20@gmail.com>
2025-10-27 20:02:32 -0300

    tercer commit

--- pages/galeria.js
+++ pages/galeria.js
@@ -1,12 +1,12 @@
 const imagenesIndex = [
-  { id: 1, nombre: "", image: "../assets/1.jpeg" },
-  { id: 2, nombre: "", image: "../assets/2.jpeg" },
-  { id: 3, nombre: "", image: "../assets/3.jpeg" },
-  { id: 4, nombre: "", image: "../assets/4.jpeg" },
-  { id: 5, nombre: "", image: "../assets/5.jpeg" },
-  { id: 6, nombre: "", image: "../assets/6.jpeg" },
-  { id: 7, nombre: "", image: "../assets/7.jpeg" },
-  { id: 8, nombre: "", image: "../assets/8.jpeg" }
+  { id: 1, nombre: "", image: "../assets/1.jpg" },
+  { id: 2, nombre: "", image: "../assets/2.jpg" },
+  { id: 3, nombre: "", image: "../assets/3.jpg" },
+  { id: 4, nombre: "", image: "../assets/4.jpg" },
+  { id: 5, nombre: "", image: "../assets/5.jpg" },
+  { id: 6, nombre: "", image: "../assets/6.jpg" },
+  { id: 7, nombre: "", image: "../assets/7.jpg" },
+  { id: 8, nombre: "", image: "../assets/8.jpg" }
 ];
 
 export default imagenesIndex;
